ba39a38913cc32be6dfd5881efa14584aa99d6ad
roman <tsoyroman29@gmail.com>
2023-12-16 23:43:20 +0900

    task6 added

---
+++ lesson6/data.js
@@ -0,0 +1,45 @@
+const dataInfo = `[
+    {
+        "image" : "img/Rectangle 15 copy (2).png",
+        "btn" : "img/Forma 1 copy.svg",
+        "header" : "ELLERY X M'O CAPSULE",
+        "dsc" : "Known for her sculptural takes on traditional tailoring,Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price" : "$52.00"
+    },
+    {
+        "image" : "img/Rectangle 15 copy (3).png",
+        "btn" : "img/Forma 1 copy.svg",
+        "header" : "ELLERY X M'O CAPSULE",
+        "dsc" : "Known for her sculptural takes on traditional tailoring,Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price" : "$52.00"
+    },
+    {
+        "image" : "img/Rectangle 15 copy (4).png",
+        "btn" : "img/Forma 1 copy.svg",
+        "header" : "ELLERY X M'O CAPSULE",
+        "dsc" : "Known for her sculptural takes on traditional tailoring,Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price" : "$52.00"
+    },
+    {
+        "image" : "img/Rectangle 15 copy (5).png",
+        "btn" : "img/Forma 1 copy.svg",
+        "header" : "ELLERY X M'O CAPSULE",
+        "dsc" : "Known for her sculptural takes on traditional tailoring,Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price" : "$52.00"
+    },
+    {
+        "image" : "img/Rectangle 15 copy (6).png",
+        "btn" : "img/Forma 1 copy.svg",
+        "header" : "ELLERY X M'O CAPSULE",
+        "dsc" : "Known for her sculptural takes on traditional tailoring,Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price" : "$52.00"
+    },
+    {
+        "image" : "img/Rectangle 15 copy (1).png",
+        "btn" : "img/Forma 1 copy.svg",
+        "header" : "ELLERY X M'O CAPSULE",
+        "dsc" : "Known for her sculptural takes on traditional tailoring,Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price" : "$52.00"
+    }
+
+]`;
